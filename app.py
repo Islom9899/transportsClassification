f21aa8d4e00aac390a8cf31e728f3f7582b53503
Mansurov_Islom <islom9899@example.com>
2025-06-12 01:32:39 +0900

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# title
+st.title('교통 수단을 분류하는 모델')
+
+# 사진 입력
+file = st.file_uploader('사진 입력',type=['png','jpeg','gif','svg'])
+if file:
+    st.image(file)
+    # PIL convert
+    img = PILImage.create(file)
+    model = load_learner('transport_model.pkl')
+
+    #prediction
+    pred,pred_id,probs = model.predict(img)
+    st.success(f"예측 결과: {pred}")
+    st.info(f"확률: {probs[pred_id]*100:.1f}%")
+    
+    # plotting
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
